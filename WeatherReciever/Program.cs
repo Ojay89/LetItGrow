556bf88f5a25286be44df09a1a678922b650d827
Tali0139 <47971571+Tali0139@users.noreply.github.com>
2020-05-06 14:51:00 +0200

    brugte En anden hjemmeside og fik 200 OK response, kan stadig ikke gemme på vores egen

--- WeatherReciever/Program.cs
+++ WeatherReciever/Program.cs
@@ -18,16 +18,15 @@ namespace WeatherReciever
     {
        
 
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             Console.WriteLine("Hello im waiting for data...");
             Console.ReadLine();
             UdpClient udpRecieveData = new UdpClient(11111);
 
             IPEndPoint MyEndPoint = new IPEndPoint(IPAddress.Any, 0);
-            string itemWebAPI = "https://letitgrowweather.azurewebsites.net/api/items";
-
-            Console.WriteLine("Its Blocked");
+            
+            Console.WriteLine("Getting ready to print and post data....");
 
 
             try
@@ -35,12 +34,11 @@ namespace WeatherReciever
                 while (true)
                 {
                     Byte[] receiveBytes = udpRecieveData.Receive(ref MyEndPoint);
-
                     string receiveData = Encoding.ASCII.GetString(receiveBytes);
 
-                   Console.WriteLine("Data:" + " " + receiveData.ToString());
-                    //Parsedvalue and post
+                    Console.WriteLine("Data:" + " " + receiveData.ToString());
 
+                    //Split received data string into substring, each denoting a specific measurement          
                     String[] subStrings = receiveData.Split(" | ", 10);
                     string dateTimeString = subStrings[1];
                     string location = subStrings[3];
@@ -48,12 +46,16 @@ namespace WeatherReciever
                     string rainString = subStrings[7];
                     string windSpeedString = subStrings[9];
 
+                    //Parsing substrings, so that each measurement is parsed into the right type (if not a string)
                     DateTime dateTime = DateTime.Parse(dateTimeString);
                     int randomTemperature = Int32.Parse(randomTemperatureString);
                     int rain = Int32.Parse(rainString);
                     int windSpeed = Int32.Parse(windSpeedString);
+
+                    //Creating new object from received data
                     Measurement newMeasurement = new Measurement { DeviceLocation = location, MeasureTime = dateTime, Rain = rain, RandomTemperature = randomTemperature, WindSpeed = windSpeed };
 
+                    //Calling postmethod on the object
                     PostMeasurementHttpTask(newMeasurement);
 
                     Thread.Sleep(200);
@@ -68,12 +70,14 @@ namespace WeatherReciever
 
         public static async void PostMeasurementHttpTask(Measurement nm)
         {
-            string ItemWebApiBase = "https://letitgrowweather.azurewebsites.net/";
+            //Virker med denne hjemmeside, men ikke den hjemmeside vi selv har lavet i Azure til projektet
+            //letitgrowweather.azurewebsites.net/
+            string ItemWebApiBase = "https://restitemserviceitem.azurewebsites.net/";
 
-           using (HttpClient client = new HttpClient())
+            using HttpClient client = new HttpClient();
             {
-                string newItemJson = JsonConvert.SerializeObject(nm);
-                var content = new StringContent(newItemJson, Encoding.UTF8, ("application/json"));
+                string newMeasurementJson = JsonConvert.SerializeObject(nm);
+                var content = new StringContent(newMeasurementJson, Encoding.UTF8, ("application/json"));
                 client.BaseAddress = new Uri(ItemWebApiBase);
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -87,9 +91,6 @@ namespace WeatherReciever
                 Debug.WriteLine(httpResponseBody);
             }
 
-            Console.WriteLine("*****Get all items for verification*****");
-            string ItemWebApi = "https://letitgrowweather.azurewebsites.net/";
-            
         }
     }
 
