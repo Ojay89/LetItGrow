0c4bd2610e195feec64ae5d4f85fc993539684e7
Oliver Eierstrand <oliv1115@edu.easj.dk>
2020-05-14 11:39:12 +0200

    Unit test til 2. første userstories lavet

--- LetItGrowUITest/UnitTest1.cs
+++ LetItGrowUITest/UnitTest1.cs
@@ -37,5 +37,38 @@ namespace LetItGrowUITest
             IWebElement plantCard = wait.Until(d => d.FindElement(By.Id("plantDeck")));
             Assert.IsTrue(plantCard.Text.Contains("southern red oak"));
         }
+
+        //[TestMethod] //lav den når vi ikke har opbrugt vores azure guldbank
+        //public void TestWeatherInRealTime()
+        //{
+        //    //Denne test har virket da jeg lavede den. Men da data bliver sendt hver 3. sekund vil testen faile.
+        //    _driver.Navigate().GoToUrl("http://localhost:3000/");
+        //    WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+        //    _driver.Manage().Window.Maximize();
+
+
+            
+        //}
+
+        [TestMethod]
+        public void TestSearchButtonInNav()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:3000/");
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            _driver.Manage().Window.Maximize();
+
+            IWebElement searchElement = wait.Until(d => d.FindElement(By.Id("SearchBar")));
+            searchElement.Click();
+            searchElement.SendKeys("Oak");
+            IWebElement searchButtonElement = wait.Until(d => d.FindElement(By.Id("getSearchPlantsButton")));
+            searchButtonElement.Click();
+
+            IWebElement plantCard = wait.Until(d => d.FindElement(By.Id("plantDeck")));
+            Assert.IsTrue(plantCard.Text.Contains("European turkey oak"));
+        }
+
+
+
+
     }
 }
