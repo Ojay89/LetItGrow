a37e6af5947372a76b9de0c110f2f5cf160cb9dd
oliv1115 <48084927+oliv1115@users.noreply.github.com>
2020-05-19 14:45:14 +0200

    updated ui test

--- LetItGrowUITest/UITest.cs
+++ LetItGrowUITest/UITest.cs
@@ -36,28 +36,27 @@ namespace LetItGrowUITest
             Assert.IsTrue(plantCard.Text.Contains("southern red oak"));
         }
 
-        //[TestMethod] 
-        //public void TestWeatherInRealTime() //VIrker ikke pt, fordi at den div weather data ligger i bliver gemt fra start af og bliver først vist efter vi logger ind. 
-        //    //Det kan chrome åbenbart ikke finde ud af???
-        //{
-        //    _driver.Navigate().GoToUrl("http://localhost:3000/MyPage.html");
-        //    WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
-        //    _driver.Manage().Window.Maximize();
+        [TestMethod]
+        public void TestWeatherInRealTime() 
+        {
+            _driver.Navigate().GoToUrl("http://localhost:3000/MyPage.html");
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+            _driver.Manage().Window.Maximize();
 
-        //    IWebElement usernameInput = wait.Until(d => d.FindElement(By.Id("username")));
-        //    usernameInput.Click();
-        //    usernameInput.SendKeys("nikolaj");
+            IWebElement usernameInput = wait.Until(d => d.FindElement(By.Id("username")));
+            usernameInput.Click();
+            usernameInput.SendKeys("nikolaj");
 
-        //    IWebElement passwordInput = wait.Until(d => d.FindElement(By.Id("password")));
-        //    passwordInput.Click();
-        //    passwordInput.SendKeys("pwd");
+            IWebElement passwordInput = wait.Until(d => d.FindElement(By.Id("password")));
+            passwordInput.Click();
+            passwordInput.SendKeys("pwd");
 
-        //    IWebElement loginButton = wait.Until(d => d.FindElement(By.Id("loginUserButton")));
-        //    loginButton.Click();
+            IWebElement loginButton = wait.Until(d => d.FindElement(By.Id("loginUserButton")));
+            loginButton.Click();
 
-        //    IWebElement weatherDataInRealTime = wait.Until(d => d.FindElement(By.Id("weatherData")));
-        //    Assert.IsTrue(weatherDataInRealTime.Text.Contains("location 2"));
-        //}
+            IWebElement weatherDataInRealTime = wait.Until(d => d.FindElement(By.Id("weatherData")));
+            Assert.IsTrue(weatherDataInRealTime.Text.Contains("Backyard"));
+        }
 
         [TestMethod]
         public void TestSearchButtonInNav()
